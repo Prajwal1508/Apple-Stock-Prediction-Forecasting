63d5b43298ffc67f2c2f1af939f97a0aeedfda43
Prajwal1508 <116496922+Prajwal1508@users.noreply.github.com>
2023-09-08 03:09:07 +0530

    Update Forecast.py

--- Forecast.py
+++ Forecast.py
@@ -5,52 +5,37 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import altair as alt
 
-#import datetime
-#from datetime import datetime
-#alt.themes.enable("streamlit")
-
-
-data_close = load(open('data_close.sav','rb'))
+# Load your data_close here
+data_close = load(open('data_close.sav', 'rb'))
 
 st.title('Apple Stock Forecasting')
 
+# Input for the number of days
+periods = st.number_input('Number of Days', min_value=1)
 
+# Generate date range based on the input
+date_range = pd.date_range(start='2020-01-01', periods=periods, freq='B')
+date_df = pd.DataFrame(date_range, columns=['Date'])
 
-#date = st.date_input('Date',min_value=datetime.datetime(2020,1,1))
-
-periods = st.number_input('Number of Days',min_value=1)
-
-datetime = pd.date_range('2020-01-01', periods=periods,freq='B')
-date_df = pd.DataFrame(datetime,columns=['Date'])
-
-#datetime = pd.date_range(date, periods=periods,freq='B')
-#date_df = pd.DataFrame(datetime,columns=['Date'])   
-
-
-
-model_sarima_final = sm.tsa.SARIMAX(data_close.Close,order=(2,1,0),seasonal_order=(1,1,0,63))
+# Create SARIMA model and forecast
+model_sarima_final = sm.tsa.SARIMAX(data_close.Close, order=(2, 1, 0), seasonal_order=(1, 1, 0, 63))
 sarima_fit_final = model_sarima_final.fit()
-forecast = sarima_fit_final.predict(len(data_close),len(data_close)+periods-1)
-forecast_df = pd.DataFrame(forecast)
-forecast_df.columns = ['Close']
+forecast = sarima_fit_final.predict(len(data_close), len(data_close) + periods - 1)
+forecast_df = pd.DataFrame(forecast, index=date_df['Date'], columns=['Close'])
 
-data_forecast = forecast_df.set_index(date_df.Date)
-st.write(data_forecast)
+# Display the forecasted data
+st.write(forecast_df)
 
-#fig = plt.plot(data_forecast, label='Forecast',color='red')
-#st.line_chart(data_forecast)
-
-fig,ax = plt.subplots()
-ax.plot(data_forecast)
+# Create and display a line plot using Matplotlib
+fig, ax = plt.subplots()
+ax.plot(forecast_df.index, forecast_df['Close'])
 ax.set_title('Apple Stock Forecast')
 ax.set_xlabel('Date')
 ax.set_ylabel('Stock Price')
 ax.grid(True)
+
+# Display the Matplotlib figure using st.pyplot
 st.pyplot(fig)
-
-                     
-                      
-
 
 
 
